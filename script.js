44b4f4e6e1d5a533bfebad939c202272bc1c101b
Shivane Rana <shivanerana2004@gmail.com>
2024-08-15 19:14:04 +0530

    js logic for monthly repayment finished

--- script.js
+++ script.js
@@ -1,8 +1,15 @@
-let monthlyPayment = prompt("");
-let principal;
-let monthlyInterestRate;
-let timespan;
+let principal = prompt("Enter total amount of loan");
+let anualInterestRate = prompt("Enter anual interest rate")/100;
+let timespan = prompt("Enter mortgage timespan");
+
+const numberOfPayment = timespan*12;
+const monthlyInterestRate = anualInterestRate/12;
 
 let numerator;
 let denominator;
 
+numerator = monthlyInterestRate*Math.pow((1+monthlyInterestRate),numberOfPayment);
+denominator = Math.pow((1+monthlyInterestRate),numberOfPayment)-1;
+
+const monthlyMortgageAmount = principal*(numerator/denominator);
+alert(monthlyMortgageAmount.toFixed(2));
