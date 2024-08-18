81e05601dad4380c66d8b544aee2a4501e5427d3
Shivane Rana <shivanerana2004@gmail.com>
2024-08-18 20:23:53 +0530

    removed logic for changin background color

--- script.js
+++ script.js
@@ -1,19 +1,3 @@
-const theme = document.querySelector(".theme");
-const themeName = document.querySelector(".themeName");
-const main = document.querySelector(".main");
-
-
-// changes theme of the page
-theme.addEventListener("click",() => { 
-    document.documentElement.classList.toggle("dark");
-    if(document.documentElement.className === "dark"){
-        themeName.textContent = "Dark ";
-    }else{
-        themeName.textContent = "Light ";
-    }
-})
-
-
 // let principal = prompt("Enter total amount of loan");
 // let anualInterestRate = prompt("Enter anual interest rate")/100;
 // let timespan = prompt("Enter mortgage timespan");
