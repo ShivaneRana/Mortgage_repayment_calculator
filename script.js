aeb66708bb6a5a929f0dc4837e806a3247147607
Shivane Rana <shivanerana2004@gmail.com>
2024-08-21 08:36:20 +0530

    variable created

--- script.js
+++ script.js
@@ -1,4 +1,4 @@
-// let principal = prompt("Enter total amount of loan");
+// `let principal = prompt("Enter total amount of loan");
 // let anualInterestRate = prompt("Enter anual interest rate")/100;
 // let timespan = prompt("Enter mortgage timespan");
 
@@ -13,3 +13,7 @@
 // denominator = Math.pow((1+monthlyInterestRate),numberOfPayment)-1;
 // const monthlyMortgageAmount = principal*(numerator/denominator);
 // alert(monthlyMortgageAmount.toFixed(2));
+
+const mortgageAmount = document.querySelector(".second"); //this is for amount div not input
+const mortgageTerm = document.querySelector(".third1"); //this is for term div not input
+const mortgageInterest = document.querySelector(".third2"); //this is for rate div no input
