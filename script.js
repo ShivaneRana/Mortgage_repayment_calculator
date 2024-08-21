723cd7ba4167bee95d5ac88854fdc1c74382b39b
Shivane Rana <shivanerana2004@gmail.com>
2024-08-21 22:09:05 +0530

    logic changes and typo fixes

--- script.js
+++ script.js
@@ -6,6 +6,8 @@ const termValue = document.querySelector(".term"); //mortgage term input
 const interestValue = document.querySelector(".interest"); //mortgage interest input
 const type = document.querySelectorAll(".choice"); //mortgage type input
 const calculate = document.querySelector(".submit"); //this is for calculate button
+const result = document.querySelector(".result"); //this is for result div
+const clear = document.querySelector(".clear");
 
 // this is to retrieve the value of data-uwu from .choice class
 type.forEach((item) => {
@@ -14,7 +16,7 @@ type.forEach((item) => {
     })
 })
 
-calculate.addEventListener("click",() => {
+function calcM(){
     let principal = principalValue.value;
     let anualInterestRate = interestValue.value/100;
     let timespan = termValue.value;
@@ -28,8 +30,12 @@ calculate.addEventListener("click",() => {
     // calculation for monthly repayement 
     numerator = monthlyInterestRate*Math.pow((1+monthlyInterestRate),numberOfPayment);
     denominator = Math.pow((1+monthlyInterestRate),numberOfPayment)-1;
-    const monthlyMortgageAmount = principal*(numerator/denominator);
-    alert(monthlyMortgageAmount.toFixed(2));    
+    let monthlyMortgageAmount = principal*(numerator/denominator);
+    monthlyMortgageAmount = monthlyMortgageAmount.toFixed(2);
+    const total = ((monthlyMortgageAmount*12)*timespan).toFixed(2);
+    return {monthlyMortgageAmount,total};
+}
 
-    
+calculate.addEventListener("click",() => {
+    calcM();
 })
